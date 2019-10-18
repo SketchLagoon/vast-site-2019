5101b3a414c0796feb668692a1725fb29629702b
Alex Wright <jawright94@gmail.com>
2019-10-17 21:18:17 -0400

    connected airtable form to giveaways page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./home/home";
 import Services from "./services/services";
+import Giveaways from "./giveaways/giveaways"
 import LabelBottomNavigation from "./mobile-nav/mobile-nav";
 import WOW from "wowjs";
 import "./home/App.css";
@@ -16,6 +17,7 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
+        <Route path={"/giveaways"} component={Giveaways} />
         <Route path={"/services"} component={Services} />
         <Route exact path={"/"} component={Home} />
         <LabelBottomNavigation className="wow fadeInUp nav"></LabelBottomNavigation>
---
+++ src/giveaways/giveaways.jsx
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import WOW from "wowjs";
+import "../home/App.css";
+
+// require('dotenv').config()
+
+const Airtable = require("airtable");
+// var AIRTABLE_API_KEY = process.env.REACT_APP_AIRTABLE_API_KEY;
+// var AIRTABLE_BASE_ID = process.env.REACT_APP_AIRTABLE_BASE_ID;
+const base = new Airtable({ apiKey: "key2rS5KPkm5x8R7I" }).base(
+  "applPoA2NeoQ18YDa"
+);
+
+class Giveaways extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      giveaways: []
+    };
+  }
+
+  componentDidMount = () => {
+    // console.log(AIRTABLE_API_KEY)
+
+    const getAirtableData = cb =>
+      base("site-giveaways")
+        .select({
+          // Selecting the first 100 records in Grid view:
+          maxRecords: 100,
+          view: "Grid view"
+        })
+        .eachPage(
+          function page(records, fetchNextPage) {
+            // This function (`page`) will get called for each page of records.
+            const airtableGiveaways = [];
+
+            records.forEach(function(record) {
+              airtableGiveaways.push(record._rawJson.fields);
+              cb(airtableGiveaways);
+              console.log(airtableGiveaways);
+            });
+
+            // To fetch the next page of records, call `fetchNextPage`.
+            // If there are more records, `page` will get called again.
+            // If there are no more records, `done` will get called.
+            fetchNextPage();
+          },
+          function done(err) {
+            if (err) {
+              console.error(err);
+              return;
+            } else {
+            }
+          }
+        );
+
+    getAirtableData(airtableGiveaways => {
+      this.setState({
+        giveaways: airtableGiveaways
+      });
+      // console.log(this.state)
+    });
+  };
+
+  render() {
+    const givewaysGrid = this.state.giveaways.map(function(giveaway) {
+      const wow = new WOW.WOW();
+      wow.init();
+
+      return (
+        <Grid item xs={12} sm={4}>
+          <a href={giveaway.gleamUrl}>
+            <div className="giveaway-card">
+              <img
+                src={giveaway.imgUrl}
+                alt="giveaway brand here"
+                className="giveaway-card-img"
+              />
+              <div className="giveaway-card-text">
+                <h1 className="giveaway-card-text-title">{giveaway.client}</h1>
+                <p className="giveaway-card-text-date">
+                  Winner Drawn: {giveaway.endDate}
+                </p>
+                <p className="giveaway-card-text-details">{giveaway.desc}</p>
+              </div>
+            </div>
+          </a>
+        </Grid>
+      );
+    });
+
+    return (
+      <Container fixed className="content">
+        <p
+          className="wow fadeIn instructions"
+          data-wow-offset="12"
+          data-wow-delay="1.2s"
+        >
+          Tap Image To Enter
+        </p>
+        <Grid container spacing={4} className="wow fadeInRight giveaway-grid">
+          {givewaysGrid}
+        </Grid>
+      </Container>
+    );
+  }
+}
+
+export default Giveaways;
